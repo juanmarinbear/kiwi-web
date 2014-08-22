71cd53d21368e89331a198ddefc2c7f1f1665616
Juan Marin <juan@codecery.com>
2014-08-22 18:28:53 -0500

    Finishes privacy module.

--- src/app/company/privacy/privacy.js
+++ src/app/company/privacy/privacy.js
@@ -4,4 +4,7 @@ angular.module('kiwiWeb')
 .controller('PrivacyCtrl', function ($scope, lang) {
   $scope.lang = lang.data;
   $scope.changeTitle($scope.lang.title);
+  $scope.isString = function(section) {
+    return angular.isString(section);
+  }
 });
