26038ef791c2d6a4e59c7b29cfe8fe0ccfc73497
Juan Marin <juan@codecery.com>
2014-09-01 15:41:38 -0500

    Implements basic transitions.

--- src/app/main/dedicated/dedicated.js
+++ src/app/main/dedicated/dedicated.js
@@ -4,4 +4,5 @@ angular.module('kiwiWeb')
 .controller('DedicatedCtrl', function ($scope, lang) {
   $scope.lang = lang.data;
   $scope.changeTitle($scope.lang.title);
+  $scope.pageClass = 'main-dedicated';
 });
--- src/app/main/products/products.js
+++ src/app/main/products/products.js
@@ -4,4 +4,5 @@ angular.module('kiwiWeb')
 .controller('ProductsCtrl', function ($scope, lang) {
   $scope.lang = lang.data;
   $scope.changeTitle($scope.lang.title);
+  $scope.pageClass = 'main-products';
 });
