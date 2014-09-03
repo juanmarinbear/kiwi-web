76a1ddec0c03b95ae19628bbd295ebb1b33640d3
Juan Marin <juan@codecery.com>
2014-09-03 17:30:08 -0500

    Removes transition logic from main controller.

--- src/app/main/main.js
+++ src/app/main/main.js
@@ -1,31 +1,9 @@
 'use strict';
 
 angular.module('kiwiWeb')
-.controller('MainCtrl', function ($scope, $state, lang) {
+.controller('MainCtrl', ['$scope', '$state', 'lang', function ($scope, $state, lang) { 
   $scope.lang = lang.data;
   $scope.changeTitle($scope.lang.title);
   $scope.pageClass = '';
-  $scope.transition = {};
-  $scope.transition.baseDuration = 6;
 
-  var sections = [ 
-    'main.landing',
-    'main.products',
-    'main.residential',
-    'main.business',
-    'main.dedicated',
-    'main.metromesh'
-  ];
-
-  $scope.$on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams) {
-
-    var fromIndex, toIndex, direction, steps;
-
-    toIndex = sections.indexOf(toState.name);
-    fromIndex = sections.indexOf(fromState.name);
-    direction = toIndex > fromIndex  ? 1 : -1;
-
-    $scope.transition.css = direction > 0 ? 'up' : 'down';
-  });
-
-});
+}]);
