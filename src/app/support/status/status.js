8a76dff7787caa9ed837075a9e47c5932f6e5331
Juan Marin <juan@codecery.com>
2014-09-10 22:50:23 -0500

    Renames faq to status.

--- src/app/support/status/status.js
+++ src/app/support/status/status.js
@@ -1,6 +1,6 @@
 'use strict';
 
-kiwiWeb.controller('FaqCtrl', function ($scope, $stateParams, lang) {
+kiwiWeb.controller('StatusCtrl', function ($scope, $stateParams, lang) {
   $scope.lang = lang.data;
   $scope.changeTitle($scope.lang.title);
 });
