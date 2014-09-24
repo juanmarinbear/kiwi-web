9fe4a1aebf008edf8bcb0249801dc2c7461014b5
Juan Marin <juan@codecery.com>
2014-09-24 16:35:06 -0500

    Adds support module.

---
+++ src/app/support/support.js
@@ -0,0 +1,48 @@
+'use strict';
+
+kiwiWeb.controller('SupportCtrl', function ($scope, $stateParams, $http, Parse, lang) {
+  $scope.lang = lang.data;
+  $scope.changeTitle($scope.lang.title);
+  $scope.data = {};
+
+  var zendesk = {};
+  zendesk.username = 'kiwiadmin@kiwinetworks.com';
+  zendesk.token = '9UWLTlGmIWLsxSMWqQSVWRo2GxYO0gsKXaXEVTUO';
+  zendesk.api = 'https://kiwinetworks.zendesk.com/api/v2';
+
+  $http.defaults.useXDomain = true;
+  delete $http.defaults.headers.common['X-Requested-With'];
+
+  $scope.submit = function() {
+
+    if($scope.support.$valid) {
+      $scope.loading = true;
+      $scope.success = true;
+      $scope.loading = false;
+      $scope.$apply();
+    }
+    $scope.submitted = true;
+/*
+    $http({
+      method: 'GET', 
+      url: $scope.zendesk.api + '/users/me.json',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Basic ' + btoa(zendesk.username + '/token:' + zendesk.token)
+      }
+      }).
+      success(function(data, status, headers, config) {
+        console.log('Success!');
+        console.log(data);
+        console.log(status);
+        console.log(config);
+      }).
+      error(function(data, status, headers, config) {
+        console.log('Error!');
+        console.log(data);
+        console.log(status);
+        console.log(config);
+      });
+*/
+  };
+});
---
+++ src/app/support/support.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('SupportCtrl', function(){
+  it('contains spec with expectation', function() {
+    expect(true).toBe(true);
+  });
+});
