f356ef7f7fe7cb6658df4992a74cdbe18b508c3c
Juan Marin <juan@codecery.com>
2014-09-24 15:14:47 -0500

    Adds markup and logic to contact form.

--- src/app/contact/contact.js
+++ src/app/contact/contact.js
@@ -3,4 +3,18 @@
 kiwiWeb.controller('ContactCtrl', function ($scope, lang) {
   $scope.lang = lang.data;
   $scope.changeTitle($scope.lang.title);
+  $scope.data = {};
+
+  $scope.submit = function() {
+
+    if($scope.contact.$valid) {
+      $scope.loading = true;
+      $scope.success = true;
+      alert('Form is valid!');
+      $scope.loading = false;
+      $scope.$apply();
+    }
+    $scope.submitted = true;
+  }
+
 });
--- src/app/contact/contactForm/contactForm.spec.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-describe('ContactFormCtrl', function(){
-  it('contains spec with expectation', function() {
-    expect(true).toBe(true);
-  });
-});
