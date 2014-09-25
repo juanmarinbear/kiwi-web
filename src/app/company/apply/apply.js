000ce521acd39492b548d30bc70ce7c6ae13adab
Juan Marin <juan@codecery.com>
2014-09-25 14:16:34 -0500

    Creates apply module.

---
+++ src/app/company/apply/apply.js
@@ -0,0 +1,19 @@
+'use strict';
+
+kiwiWeb.controller('ApplyCtrl', function ($scope, lang) {
+  $scope.lang = lang.data;
+  $scope.changeTitle($scope.lang.title);
+  $scope.data = {};
+
+  $scope.submit = function() {
+
+    if($scope.contact.$valid) {
+      $scope.loading = true;
+      $scope.success = true;
+      $scope.loading = false;
+      $scope.$apply();
+    }
+    $scope.submitted = true;
+  };
+
+});
---
+++ src/app/company/apply/apply.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('ApplyCtrl', function(){
+  it('contains spec with expectation', function() {
+    expect(true).toBe(true);
+  });
+});
