a4d2e714f228c1b858b9225ca45749a5564d29fc
Juan Marin <juan@codecery.com>
2014-09-26 22:59:44 -0500

    Finishes signUp control flow (for real this time).

--- src/app/customer/pay/pay.js
+++ src/app/customer/pay/pay.js
@@ -4,17 +4,26 @@ kiwiWeb.controller('PayCtrl', function ($scope, $filter, lang) {
   $scope.lang = lang.data;
   $scope.changeTitle($scope.lang.title);
   $scope.data = {};
-  $scope.data.client;
-  $scope.data.user;
-  $scope.data.steps = {
-    lookUpSerial: true,
-    submitMobile: false,
-    authenticate: false
-  };
+  $scope.data.serialLookUp = {};
+  $scope.data.serialLookUp.active = true;
+  $scope.data.mobileSignUp = {};
+  $scope.data.mobileSignUp.active = false;
+  $scope.data.mobileSignIn = {};
+  $scope.data.mobileSignIn.active = true;
+  $scope.data.mobileSignUpAuth = {};
+  $scope.data.mobileSignUpAuth.active = false;
+  $scope.data.mobileSignInAuth = {};
+  $scope.data.mobileSignInAuth.active = false;
 
-  $scope.data.users = [
+  $scope.data.serialLookUpOriginal = angular.copy($scope.data.serialLookUp);
+  $scope.data.mobileSignUpOriginal = angular.copy($scope.data.mobileSignUp);
+  $scope.data.mobileSignInOriginal = angular.copy($scope.data.mobileSignIn);
+  $scope.data.mobileSignUpAuthOriginal = angular.copy($scope.data.mobileSignUpAuth);
+  $scope.data.mobileSignInAuthOriginal = angular.copy($scope.data.mobileSignInAuth);
+
+  $scope.users = [
     {
-      "serial" : "123456789098",
+      "serial" : "222222222222",
       "name" : "Juan Marin Bear",
       "users" : [
         {
@@ -26,49 +35,72 @@ kiwiWeb.controller('PayCtrl', function ($scope, $filter, lang) {
       ]
     },
     {
-      "serial" : "987654321234",
+      "serial" : "111111111111",
       "name" : "Dulce Maria Carral",
       "users" : []
     }
   ];
 
-  $scope.originalData = angular.copy($scope.data);
+  $scope.pin = "1234";
 
-  $scope.reset = function() {
-    $scope.data = angular.copy($scope.originalData);
-    if($scope.lookUpSerial) $scope.lookUpSerial.$setPristine();
-  }
+  $scope.resetSerialLookUp = function() {
+    $scope.data.serialLookUp = angular.copy($scope.data.serialLookUpOriginal);
+    $scope.data.mobileSignUp = angular.copy($scope.data.mobileSignUpOriginal);
+  };
 
-  $scope.submitSerial = function() {
+  $scope.resetMobileSignUp = function() {
+    $scope.data.mobileSignUp = angular.copy($scope.data.mobileSignUpOriginal);
+    $scope.data.mobileSignUp.active = true;
+    $scope.data.mobileSignUpAuth = angular.copy($scope.data.mobileSignUpAuthOriginal);
+  };
 
-    if($scope.lookUpSerial.$valid) {
-      $scope.loading = true;
-      $scope.data.steps.lookUpSerial = false;
-      $scope.data.client = $filter('filter')($scope.data.users, {serial: $scope.data.serial}, true)[0];
-      if($scope.data.client) {
-        if($scope.data.client.users.length) {
-          $scope.data.user = $filter('filter')($scope.data.client.users, {role: 'admin'}, true)[0];
+  $scope.serialLookUpSubmit = function() {
+
+    $scope.loading = true;
+
+    if($scope.serialLookUp.$valid) {
+      $scope.data.serialLookUp.client = $filter('filter')($scope.users, {serial: $scope.data.serialLookUp.serial}, true)[0];
+      if($scope.data.serialLookUp.client) {
+        if($scope.data.serialLookUp.client.users.length) {
+          $scope.data.serialLookUp.user = $filter('filter')($scope.data.serialLookUp.client.users, {role: 'admin'}, true)[0];
         } else {
-          $scope.data.steps.submitMobile = true;
+          $scope.data.serialLookUp.active = false;
+          $scope.data.mobileSignUp.active = true;
         }
       }
-      $scope.loading = false;
     }
 
-    $scope.data.lookUpSerialSubmitted = true;
+    $scope.data.serialLookUp.submitted = true;
+    $scope.loading = false;
   };
 
-  $scope.submitMobile = function() {
+  $scope.mobileSignUpSubmit = function() {
 
-    if($scope.submitMobile.$valid) {
-      $scope.loading = true;
-      $scope.success = true;
-      $scope.loading = false;
+    $scope.loading = true;
+
+    if($scope.mobileSignUp.$valid) {
+      $scope.data.mobileSignUp.active = false;
+      $scope.data.mobileSignUpAuth.active = true;
     }
-    $scope.submitted = true;
+
+    $scope.data.mobileSignUp.submitted = true;
+    $scope.loading = false;
   };
 
-  $scope.authenticate = function() {
-  
+  $scope.mobileSignUpAuthSubmit = function() {
+
+    $scope.loading = true;
+
+    if($scope.mobileSignUpAuth.$valid) {
+      if($scope.data.mobileSignUpAuth.password === "1234") {
+        $scope.data.mobileSignUpAuth.success = true;
+        $scope.data.mobileSignUpAuth.active = false;
+      } else {
+        $scope.data.mobileSignUpAuth.error = true;
+      }
+    }
+
+    $scope.data.mobileSignUpAuth.submitted = true;
+    $scope.loading = false;
   };
 });
