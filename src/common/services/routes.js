d072b408cafb0baab5a20615fecd45484e7eb8c7
Juan Marin <juan@codecery.com>
2014-09-30 23:26:02 -0500

    Adds faq transition and reorders website transitions.

--- src/common/services/routes.js
+++ src/common/services/routes.js
@@ -3,80 +3,86 @@
 kiwiWeb.factory('Routes', function() {
 
   return {
-    home: {
-      parent: 'home',
-      xOrder: 2,
-      yOrder: 0
-    },
-    products: {
-      parent: 'home',
-      xOrder: 2,
-      yOrder: 1
-    },
-    residential: {
-      parent: 'home',
-      xOrder: 2,
-      yOrder: 2
-    },
-    business: {
-      parent: 'home',
-      xOrder: 2,
-      yOrder: 3
-    },
-    dedicated: {
-      parent: 'home',
-      xOrder: 2,
-      yOrder: 4
-    },
-    metromesh: {
-      parent: 'home',
-      xOrder: 2,
-      yOrder: 5
-    },
-    sales: {
-      parent: 'form'
-    },
-    apply: {
-      parent: 'form'
-    },
     about: {
       parent: 'company',
-      xOrder: 1,
+      xOrder: 0,
       yOrder: 0
     },
     concesion: {
       parent: 'company',
-      xOrder: 1,
+      xOrder: 0,
       yOrder: 1
     },
     careers: {
       parent: 'company',
-      xOrder: 1,
+      xOrder: 0,
       yOrder: 2
+    },
+    apply: {
+      parent: 'form'
     },
     stories: {
       parent: 'company',
-      xOrder: 1,
+      xOrder: 0,
       yOrder: 3
     },
     privacy: {
       parent: 'company',
+      xOrder: 0,
+      yOrder: 4
+    },
+    home: {
+      parent: 'home',
+      xOrder: 1,
+      yOrder: 0
+    },
+    products: {
+      parent: 'home',
+      xOrder: 1,
+      yOrder: 1
+    },
+    residential: {
+      parent: 'home',
+      xOrder: 1,
+      yOrder: 2
+    },
+    business: {
+      parent: 'home',
+      xOrder: 1,
+      yOrder: 3
+    },
+    dedicated: {
+      parent: 'home',
       xOrder: 1,
       yOrder: 4
     },
-    contact: {
-      parent: 'contact',
-      xOrder: 0,
-      yOrder: 0
+    metromesh: {
+      parent: 'home',
+      xOrder: 1,
+      yOrder: 5
+    },
+    faq: {
+      parent: 'home',
+      xOrder: 1,
+      yOrder: 6 
+    },
+    sales: {
+      parent: 'form'
     },
     dashboard: {
-      parent: 'form'
+      parent: 'customer',
+      xOrder: 2,
+      yOrder: 0 
     },
     pay: {
-      parent: 'form'
+      parent: 'customer',
+      xOrder: 2,
+      yOrder: 1 
     },
     refer: {
-      parent: 'form'
+      parent: 'customer',
+      xOrder: 2,
+      yOrder: 2 
     },
     support: {
       parent: 'support',
@@ -87,6 +93,11 @@ kiwiWeb.factory('Routes', function() {
       parent: 'support',
       xOrder: 3,
       yOrder: 1
+    },
+    contact: {
+      parent: 'contact',
+      xOrder: 4,
+      yOrder: 0
     }
   };
 });
