c9febe5db4961473e6bec7a384ee14a7d9f00683
Juan Marin <juan@codecery.com>
2014-10-08 21:58:45 -0500

    Links Parse.com to Zendesk and viceversa.

--- cloud_code/cloud/main.js
+++ cloud_code/cloud/main.js
@@ -6,7 +6,8 @@ Parse.Cloud.beforeSave("Sales", function(request, response) {
   ticket = JSON.parse(ticket);
 
   if(sales.valid(ticket)) {
-    sales.save(ticket, function () {
+    sales.save(ticket, function (data) {
+      request.object.set('zendeskId', data.ticket.id.toString());
       response.success();
     }, function (error) {
       response.error(error);
@@ -14,4 +15,17 @@ Parse.Cloud.beforeSave("Sales", function(request, response) {
   } else {
     response.error('Error!');
   }
+});
+
+Parse.Cloud.afterSave("Sales", function(request) {
+  var zendeskId = request.object.get('zendeskId');
+  var ticket = {
+    ticket: {
+      external_id: request.object.id
+    }
+  };
+  sales.update(zendeskId, ticket, function (data) {
+  }, function (error) {
+    console.error("Got an error " + error.code + " : " + error.message);
+  });
 });
