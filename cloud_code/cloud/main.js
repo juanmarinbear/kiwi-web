da1eafe18e99e9f5899257024c5ce6b8af6faa0d
Juan Marin <juan@codecery.com>
2014-10-09 17:45:45 -0500

    Adds support ticket routines.

--- cloud_code/cloud/main.js
+++ cloud_code/cloud/main.js
@@ -1,4 +1,5 @@
 var sales = require('cloud/sales.js');
+var support = require('cloud/support.js');
 
 Parse.Cloud.beforeSave("Sales", function(request, response) {
 
@@ -18,6 +19,36 @@ Parse.Cloud.beforeSave("Sales", function(request, response) {
 });
 
 Parse.Cloud.afterSave("Sales", function(request) {
+  var zendeskId = request.object.get('zendeskId');
+  var ticket = {
+    ticket: {
+      external_id: request.object.id
+    }
+  };
+  sales.update(zendeskId, ticket, function (data) {
+  }, function (error) {
+    console.error("Got an error " + error.code + " : " + error.message);
+  });
+});
+
+Parse.Cloud.beforeSave("Support", function(request, response) {
+
+  var ticket = JSON.stringify(request.object);
+  ticket = JSON.parse(ticket);
+
+  if(support.valid(ticket)) {
+    support.save(ticket, function (data) {
+      request.object.set('zendeskId', data.ticket.id.toString());
+      response.success();
+    }, function (error) {
+      response.error(error);
+    });
+  } else {
+    response.error('Error!');
+  }
+});
+
+Parse.Cloud.afterSave("Support", function(request) {
   var zendeskId = request.object.get('zendeskId');
   var ticket = {
     ticket: {
