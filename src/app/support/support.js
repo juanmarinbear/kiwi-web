a457264db47e1c3ef319788e9b5730dc0f3179d1
Juan Marin <juan@codecery.com>
2014-10-09 16:53:08 -0500

    Finishes support form integration.

--- src/app/support/support.js
+++ src/app/support/support.js
@@ -1,6 +1,6 @@
 'use strict';
 
-kiwiWeb.controller('SupportCtrl', function ($scope, $stateParams, $http, lang, support) {
+kiwiWeb.controller('SupportCtrl', function ($scope, $stateParams, $http, KiwiWebApi, lang, support) {
   $scope.lang = lang.data;
   $scope.support = support.data.support;
   $scope.changeTitle($scope.lang.title);
@@ -10,7 +10,18 @@ kiwiWeb.controller('SupportCtrl', function ($scope, $stateParams, $http, lang, s
   };
   $scope.errors = {};
 
-  $scope.submit = function() {
+  $scope.submit = function () {
+    if($scope.forms.support.$valid) {
+      KiwiWebApi.support.save($scope.ticket, function (ticket) {
+        console.log('Success!');
+        $scope.success = true;
+        console.log(ticket);
+      }, function (error) {
+        $scope.error = true;
+        console.log('Error!');
+        console.log(error);
+      });
+    }
     $scope.forms.support.submitted = true;
   };
 });
