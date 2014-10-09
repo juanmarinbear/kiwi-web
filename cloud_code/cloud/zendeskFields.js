ff55071308fa3aa487887bb7089af9606159093a
Juan Marin <juan@codecery.com>
2014-10-09 17:46:16 -0500

    Adds additional field dictionary.

--- cloud_code/cloud/zendeskFields.js
+++ cloud_code/cloud/zendeskFields.js
@@ -5,9 +5,13 @@
 
 module.exports = {
   sales: '22855',
+  technical: '24019',
+  administrative: '22835',
   step: '22882395',
   outcome: '22882405',
+  supportSubject: '22884985'
   service: '22630199',
+  company: '22884735',
   contact: '22785105',
   zip: '22739659',
   state: '22630289',
