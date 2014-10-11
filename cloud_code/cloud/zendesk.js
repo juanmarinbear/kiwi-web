048a2778174c36efe02945ffde9613e6ac2f0709
Juan Marin <juan@codecery.com>
2014-10-10 22:13:19 -0500

    Simplifies functions.

---
+++ cloud_code/cloud/zendesk.js
@@ -0,0 +1,51 @@
+/*
+ * This Module:
+ * Validates Sales, Technical, Administrative, Contact, Jobs and Privacy classes object before sending to Zendesk.
+ * Formats object according to Zendesk specification.
+ * Submits object to Zendesk.
+ */
+
+var validate = require('cloud/lib/validate.min.js');
+var auth = require('cloud/auth.js');
+
+module.exports = {
+
+  valid: function (ticket, constraints) {
+    if(validate(ticket, constraints)) {
+      return false; 
+    } else {
+      return true; 
+    }
+  },
+  errors: function (ticket, constraints) {
+    return validate(ticket, constraints);
+  },
+  save: function (ticket, success, error) {
+    Parse.Cloud.httpRequest({
+      method: 'POST',
+      url: 'https://kiwinetworks.zendesk.com/api/v2/tickets.json',
+      headers: auth.zendesk.headers, 
+      body: ticket,
+      success: function (httpResponse) {
+        success(httpResponse.data); 
+      },
+      error: function(httpResponse) {
+        error('Request failed with response code ' + httpResponse.status);
+      }
+    });  
+  },
+  update: function (zendeskId, ticket, success, error) {
+    Parse.Cloud.httpRequest({
+      method: 'PUT',
+      url: 'https://kiwinetworks.zendesk.com/api/v2/tickets/' + zendeskId + '.json',
+      headers: auth.zendesk.headers, 
+      body: ticket,
+      success: function (httpResponse) {
+        success(httpResponse.data); 
+      },
+      error: function(httpResponse) {
+        error('Request failed with response code ' + httpResponse.status);
+      }
+    });  
+  }
+}
---
+++ cloud_code/cloud/zendeskFormat.js
@@ -0,0 +1,163 @@
+/*
+ * This Module:
+ * Formats ticket according to Zendesk form specification.
+ */
+
+var zendeskFields = require('cloud/zendeskFields.js')
+var foldToASCII = require('cloud/lib/fold-to-ascii.min.js')
+
+module.exports = {
+  sales: function(ticket) {
+    return {
+      ticket: {
+        requester: {
+          name: ticket.name + ' ' + ticket.last,
+          email: ticket.email,
+          locale_id: '2',
+          user_fields: {
+            mobile_mx: ticket.mobile 
+          }
+        },
+        subject: ticket.type + ' - ' + ticket.service,
+        description: ticket.type + ' - ' + ticket.service,
+        ticket_form_id: zendeskFields.sales,
+        custom_fields: [
+          { id: zendeskFields.step, value: ticket.step },
+          { id: zendeskFields.outcome, value: ticket.outcome },
+          { id: zendeskFields.service, value: foldToASCII(ticket.service).replace(/\s/g, '_').toLowerCase() },
+          { id: zendeskFields.company, value: ticket.company},
+          { id: zendeskFields.contact, value: ticket.contact },
+          { id: zendeskFields.zip, value: ticket.zip },
+          { id: zendeskFields.state, value: ticket.state },
+          { id: zendeskFields.municipality, value: ticket.municipality },
+          { id: zendeskFields.district, value: ticket.district },
+          { id: zendeskFields.street, value: ticket.street },
+          { id: zendeskFields.number, value: ticket.number },
+          { id: zendeskFields.apt, value: ticket.apt },
+          { id: zendeskFields.building, value: ticket.building },
+          { id: zendeskFields.levels, value: ticket.levels }
+        ]
+      }
+    }
+  },
+  technical: function(ticket) {
+    return {
+      ticket: {
+        requester: {
+          name: ticket.name + ' ' + ticket.last,
+          email: ticket.email,
+          locale_id: '2',
+          user_fields: {
+            mobile_mx: ticket.mobile 
+          }
+        },
+        subject: ticket.type + ' - ' + ticket.service + ' - ' + ticket.subject,
+        description: ticket.message == '' ? ticket.type + ' - ' + ticket.service + ' - ' + ticket.subject : ticket.message,
+        ticket_form_id: zendeskFields[ticket.type.toLowerCase()],
+        custom_fields: [
+          { id: zendeskFields.supportSubject, value: ticket.subject == 'Other' ? 'support_other' : foldToASCII(ticket.subject).replace(/\s/g, '_').toLowerCase() },
+          { id: zendeskFields.company, value: ticket.company},
+          { id: zendeskFields.service, value: foldToASCII(ticket.service).replace(/\s/g, '_').toLowerCase() },
+          { id: zendeskFields.contact, value: ticket.contact }
+        ]
+      } 
+    }
+  },
+  administrative: function(ticket) {
+    return {
+      ticket: {
+        requester: {
+          name: ticket.name + ' ' + ticket.last,
+          email: ticket.email,
+          locale_id: '2',
+          user_fields: {
+            mobile_mx: ticket.mobile 
+          }
+        },
+        subject: ticket.type + ' - ' + ticket.service + ' - ' + ticket.subject,
+        description: ticket.message == '' ? ticket.type + ' - ' + ticket.service + ' - ' + ticket.subject : ticket.message,
+        ticket_form_id: zendeskFields[ticket.type.toLowerCase()],
+        custom_fields: [
+          { id: zendeskFields.supportSubject, value: ticket.subject == 'Other' ? 'support_other' : foldToASCII(ticket.subject).replace(/\s/g, '_').toLowerCase() },
+          { id: zendeskFields.company, value: ticket.company},
+          { id: zendeskFields.service, value: foldToASCII(ticket.service).replace(/\s/g, '_').toLowerCase() },
+          { id: zendeskFields.contact, value: ticket.contact }
+        ]
+      }
+    }
+  },
+  contact: function(ticket) {
+    return {
+      ticket: {
+        requester: {
+          name: ticket.name + ' ' + ticket.last,
+          email: ticket.email,
+          locale_id: '2',
+          user_fields: {
+            mobile_mx: ticket.mobile 
+          }
+        },
+        subject: ticket.type + ' - ' + ticket.subject,
+        description: ticket.message,
+        ticket_form_id: zendeskFields.contactForm,
+        custom_fields: [
+          { id: zendeskFields.contactSubject, value: ticket.subject == 'Other' ? 'contact_other' : foldToASCII(ticket.subject).replace(/\s/g, '_').toLowerCase() },
+          { id: zendeskFields.company, value: ticket.company},
+          { id: zendeskFields.contact, value: ticket.contact }
+        ]
+      }
+    }
+  },
+  privacy: function(ticket) {
+    return {
+      ticket: {
+        requester: {
+          name: ticket.name + ' ' + ticket.last,
+          email: ticket.email,
+          locale_id: '2',
+          user_fields: {
+            mobile_mx: ticket.mobile 
+          }
+        },
+        subject: ticket.type + ' - ' + ticket.subject,
+        description: ticket.message,
+        ticket_form_id: zendeskFields.contactForm,
+        custom_fields: [
+          { id: zendeskFields.contactSubject, value: ticket.subject == 'Other' ? 'contact_other' : foldToASCII(ticket.subject).replace(/\s/g, '_').toLowerCase() },
+          { id: zendeskFields.company, value: ticket.company },
+          { id: zendeskFields.contact, value: ticket.contact }
+        ]
+      }
+    }
+  },
+  jobs: function(ticket) {
+    return {
+      ticket: {
+        requester: {
+          name: ticket.name + ' ' + ticket.last,
+          email: ticket.email,
+          locale_id: '2',
+          user_fields: {
+            mobile_mx: ticket.mobile 
+          }
+        },
+        subject: ticket.type + ' - ' + ticket.role,
+        description: ticket.message,
+        ticket_form_id: zendeskFields.jobs,
+        custom_fields: [
+          { id: zendeskFields.role, value: foldToASCII(ticket.role).replace(/\s/g, '_').toLowerCase() },
+          { id: zendeskFields.state, value: ticket.state },
+          { id: zendeskFields.contact, value: ticket.contact },
+          { id: zendeskFields.education, value: foldToASCII(ticket.education) },
+          { id: zendeskFields.college, value: ticket.college},
+          { id: zendeskFields.collegeUrl, value: ticket.collegeUrl },
+          { id: zendeskFields.masters, value: ticket.masters },
+          { id: zendeskFields.mastersUrl, value: ticket.mastersUrl },
+          { id: zendeskFields.phd, value: ticket.phd },
+          { id: zendeskFields.phdUrl, value: ticket.phdUrl },
+          { id: zendeskFields.experience, value: ticket.experience },
+        ]
+      } 
+    }
+  }
+}
