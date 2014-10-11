40673f7279dddc4dbca95e00edb03431dd4dac5b
Juan Marin <juan@codecery.com>
2014-10-10 22:13:45 -0500

    Simplifies service.

--- src/common/services/kiwiWebApi.js
+++ src/common/services/kiwiWebApi.js
@@ -2,69 +2,19 @@
 
 kiwiWeb.factory('KiwiWebApi', function ($http, Auth) {
   return {
-    sales: {
-      save: function(ticket, success, error) {
-        $http({
-          method: 'POST',
-          url: 'https://api.parse.com/1/classes/Sales',
-          headers: Auth.kiwiWebApi.headers,
-          data: ticket
-        }).
-        success(function(data) {
-          success(data);
-        }).
-        error(function(data) {
-          error(data);
-        });
-      }
-    },
-    support: {
-      save: function(ticket, success, error) {
-        $http({
-          method: 'POST',
-          url: 'https://api.parse.com/1/classes/Support',
-          headers: Auth.kiwiWebApi.headers,
-          data: ticket
-        }).
-        success(function(data) {
-          success(data);
-        }).
-        error(function(data) {
-          error(data);
-        });
-      }
-    },
-    contact: {
-      save: function(ticket, success, error) {
-        $http({
-          method: 'POST',
-          url: 'https://api.parse.com/1/classes/Contact',
-          headers: Auth.kiwiWebApi.headers,
-          data: ticket
-        }).
-        success(function(data) {
-          success(data);
-        }).
-        error(function(data) {
-          error(data);
-        });
-      }
-    },
-    apply: {
-      save: function(ticket, success, error) {
-        $http({
-          method: 'POST',
-          url: 'https://api.parse.com/1/classes/Jobs',
-          headers: Auth.kiwiWebApi.headers,
-          data: ticket
-        }).
-        success(function(data) {
-          success(data);
-        }).
-        error(function(data) {
-          error(data);
-        });
-      }
+    save: function(ticket, endpoint, success, error) {
+      $http({
+        method: 'POST',
+        url: 'https://api.parse.com/1/classes/' + endpoint,
+        headers: Auth.kiwiWebApi.headers,
+        data: ticket
+      }).
+      success(function(data) {
+        success(data);
+      }).
+      error(function(data) {
+        error(data);
+      });
     }
   }
 });
