170c049d3a2ffed7f8c2dac2f2aa9c8a02ba8d81
Juan Marin <juan@codecery.com>
2014-10-28 21:06:59 -0600

    Styles careers, concesion and stories.

--- src/app/company/stories/stories.js
+++ src/app/company/stories/stories.js
@@ -1,6 +1,10 @@
 'use strict';
 
-kiwiWeb.controller('StoriesCtrl', function ($scope, lang) {
+kiwiWeb.controller('StoriesCtrl', function ($scope, $sce, lang) {
   $scope.lang = lang.data;
   $scope.changeTitle($scope.lang.title);
+
+  $scope.trustHtml = function (html) {
+    return $sce.trustAsHtml(html);
+  };
 });
