cd4325a2e0cf771616215e77efa8913c6ca8b002
Juan Marin <juan@codecery.com>
2014-10-31 17:45:47 -0600

    Fixes jshint.

--- src/common/services/mediaQuery.js
+++ src/common/services/mediaQuery.js
@@ -3,12 +3,18 @@
 kiwiWeb.factory('MediaQuery', function($window) {
   return {
     screenSize: function() {
-      if($window.matchMedia('(max-width: 35.5em)').matches)
+      if($window.matchMedia('(max-width: 35.5em)').matches) {
         return 'sm';
-      if($window.matchMedia('(max-width: 48em)').matches)
+      }
+
+      if($window.matchMedia('(max-width: 48em)').matches) {
         return 'md';
-      if($window.matchMedia('(max-width: 64em)').matches)
+      }
+
+      if($window.matchMedia('(max-width: 64em)').matches) {
         return 'lg';
+      }
+
       return 'xl';
     }
   };
