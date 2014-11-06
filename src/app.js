7054ba5fcc10eb97bcd5288f4114f67ff4d826a6
Juan Marin <juan@codecery.com>
2014-11-06 15:06:28 -0600

    Uses templateProvider to load templates from js.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var kiwiWeb = angular.module('kiwiWeb', [
   'ui.router',
   'ngAnimate',
   'ngSanitize',
+  'templates',
   'languages'
 ]);
 
@@ -16,135 +17,185 @@ kiwiWeb.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider
   $stateProvider
   .state('home', {
     url: '/home',
-    templateUrl: '/home/home.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('home.html');
+    },
     controller: 'HomeCtrl'
   })
   .state('services', {
     url: '/services',
-    templateUrl: '/services/services/services.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('services/services.html');
+    },
     controller: 'ServicesCtrl'
   })
   .state('residential', {
     url: '/residential',
-    templateUrl: '/services/residential/residential.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('residential/residential.html');
+    },
     controller: 'ResidentialCtrl'
   })
   .state('business', {
     url: '/business',
-    templateUrl: '/services/business/business.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('business/business.html');
+    },
     controller: 'BusinessCtrl'
   })
   .state('dedicated', {
     url: '/dedicated',
-    templateUrl: '/services/dedicated/dedicated.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('dedicated/dedicated.html');
+    },
     controller: 'DedicatedCtrl'
   })
   .state('metromesh', {
     url: '/metromesh',
-    templateUrl: '/services/metromesh/metromesh.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('metromesh/metromesh.html');
+    },
     controller: 'MetroMeshCtrl'
   })
   .state('faq', {
     url: '/faq/:section',
-    templateUrl: '/services/faq/faq.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('faq/faq.html');
+    },
     controller: 'FaqCtrl'
   })
   .state('sales', {
     url: '/sales/:service',
-    templateUrl: '/services/sales/sales.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('sales/sales.html');
+    },
     controller: 'SalesCtrl'
   })
   .state('about', {
     url: '/about',
-    templateUrl: '/company/about/about.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('about/about.html');
+    },
     controller: 'AboutCtrl'
   })
   .state('concesion', {
     url: '/concesion',
-    templateUrl: '/company/concesion/concesion.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('concesion/concesion.html');
+    },
     controller: 'ConcesionCtrl'
   })
   .state('careers', {
     url: '/careers',
-    templateUrl: '/company/careers/careers.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('careers/careers.html');
+    },
     controller: 'CareersCtrl'
   })
   .state('apply', {
     url: '/apply/:role',
-    templateUrl: '/company/apply/apply.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('apply/apply.html');
+    },
     controller: 'ApplyCtrl'
   })
   .state('stories', {
     url: '/stories',
-    templateUrl: '/company/stories/stories.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('stories/stories.html');
+    },
     controller: 'StoriesCtrl'
   })
   .state('privacy', {
     url: '/privacy',
-    templateUrl: '/company/privacy/privacy.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('privacy/privacy.html');
+    },
     controller: 'PrivacyCtrl'
   })
   .state('unsubscribe', {
     url: '/unsubscribe',
-    templateUrl: '/company/unsubscribe/unsubscribe.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('unsubscribe/unsubscribe.html');
+    },
     controller: 'UnsubscribeCtrl'
   })
   .state('contact', {
     url: '/contact',
-    templateUrl: '/contact/contact.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('contact.html');
+    },
     controller: 'ContactCtrl'
   })
   .state('dashboard', {
     abstract: true,
     url: '/dashboard',
-    templateUrl: '/customer/dashboard/dashboard.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('dashboard/dashboard.html');
+    },
     controller: 'DashboardCtrl'
   })
   .state('dashboard.anon', {
     url: '/anon',
     views: {
       'signup' : {
-        templateUrl: '/components/signup/signup.html',
+        templateProvider: function ($templateCache) {
+          return $templateCache.get('signup/signup.html');
+        },
         controller: 'SignUpCtrl'
       },
       'signin' : {
-        templateUrl: '/components/signin/signin.html',
+        templateProvider: function ($templateCache) {
+          return $templateCache.get('signin/signin.html');
+        },
         controller: 'SignInCtrl'
       }
     }
   })
   .state('pay', {
     url: '/pay',
-    templateUrl: '/customer/pay/pay.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('pay/pay.html');
+    },
     controller: 'PayCtrl'
   })
   .state('refer', {
     abstract: true,
     url: '/refer',
-    templateUrl: '/customer/refer/refer.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('refer/refer.html');
+    },
     controller: 'ReferCtrl'
   })
   .state('refer.anon', {
     url: '/anon',
     views: {
       'signup' : {
-        templateUrl: '/components/signup/signup.html',
+        templateProvider: function ($templateCache) {
+          return $templateCache.get('signup/signup.html');
+        },
         controller: 'SignUpCtrl'
       },
       'signin' : {
-        templateUrl: '/components/signin/signin.html',
+        templateProvider: function ($templateCache) {
+          return $templateCache.get('signin/signin.html');
+        },
         controller: 'SignInCtrl'
       }
     }
   })
   .state('support', {
     url: '/support',
-    templateUrl: '/support/support/support.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('support/support.html');
+    },
     controller: 'SupportCtrl'
   })
   .state('status', {
     url: '/status',
-    templateUrl: '/support/status/status.html',
+    templateProvider: function ($templateCache) {
+      return $templateCache.get('status/status.html');
+    },
     controller: 'StatusCtrl'
   });
 }]);
