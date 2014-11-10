f9013452a3d42f2e8a1bbdec040b94b9f2e57bf5
Juan Marin <juan@codecery.com>
2014-11-10 14:41:16 -0600

    Implements basic directive.

--- src/components/vcenter/vcenter-directive.js
+++ src/components/vcenter/vcenter-directive.js
@@ -5,19 +5,18 @@ angular.module('kiwiWeb')
 
     return {
       restrict: 'A',
-      link: function (scope, element, attrs) {
+      link: function (scope, element) {
 
         var w = angular.element($window);
 
         scope.vcenter = function () {
-          console.log('Center!');
           var box = angular.element(element[0]);
           var windowHeight = $window.innerHeight;
           var headerHeight = document.querySelector('#header').offsetHeight;
           var boxHeight = element[0].offsetHeight;
           var margin = (windowHeight - headerHeight - boxHeight) / 2;
           box.css({'margin-top': margin + 'px'});
-        }
+        };
 
         $timeout(function () {
           scope.vcenter();
@@ -27,5 +26,5 @@ angular.module('kiwiWeb')
           scope.vcenter();  
         });
       }
-    }
+    };
   });
