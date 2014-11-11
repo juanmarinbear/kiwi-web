10ba5686cbfd863a4e0b18a04496736672529cc0
Juan Marin <juan@codecery.com>
2014-11-11 11:35:56 -0600

    Adds video background hide and show behavior.

--- src/app-controller.js
+++ src/app-controller.js
@@ -7,12 +7,18 @@ kiwiWeb.controller('AppCtrl', ['$scope', '$state', '$window', '$templateCache', 
   $scope.styles = {};
   $scope.langKey = 'es';
   $scope.menu = Menu;
-  $scope.menu.active = true;
   $scope.menu.current = $scope.menu.sections[0];
 
   var w = angular.element($window);
 
   $scope.language = $templateCache.get('language_default');
+
+  $timeout(function () {
+    $scope.menu.active = true;
+    $timeout(function () {
+      $scope.menu.active = false; 
+    }, 1500);
+  }, 1500);
 
   $scope.changeTitle = function (title) {
     $scope.title = title;
@@ -44,6 +50,16 @@ kiwiWeb.controller('AppCtrl', ['$scope', '$state', '$window', '$templateCache', 
 
     if(toParams === fromParams) {
       console.log('Useles JSHINT!');
+    }
+
+    if($scope.menu.active) {
+      $scope.menu.active = !$scope.menu.active;
+    }
+
+    if(toState.name === 'home') {
+      $scope.styles.home = true;
+    } else {
+      $scope.styles.home = false; 
     }
 
     $scope.styles.transition = Transition.getTransition(fromState.name, toState.name);
